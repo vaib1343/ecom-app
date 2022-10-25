36486262197747f0f2a776ff4865f97da9835744
vaibhav.varshney <vaibhav.varshney@flipkart.com>
2022-10-26 00:08:40 +0530

    auth enabled

--- src/components/SignInForm/SignInForm.jsx
+++ src/components/SignInForm/SignInForm.jsx
@@ -2,6 +2,8 @@ import './SignInForm.scss';
 import React, { useState } from 'react';
 import FormInput from '../FormInput/FormInput';
 import Button from '../Button/Button';
+import { getRedirectResult } from 'firebase/auth';
+import { auth, createUserDocumentFromAuth, signInWithGooglePopup, SignInAuthUserWithEmailPassword } from '../../utils/firebase/firebaseUtils';
 
 const defaultValue = {
     email: '',
@@ -12,6 +14,10 @@ const SignInForm = () => {
     const [formFields, setFormFields] = useState(defaultValue);
     const { email, password } = formFields;
 
+    const resetFormField = () => {
+        setFormFields(defaultValue);
+    };
+
     const handleChange = (event) => {
         const { name, value } = event.target;
         setFormFields((preState) => ({
@@ -19,9 +25,31 @@ const SignInForm = () => {
             [name]: value,
         }));
     };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            await SignInAuthUserWithEmailPassword(email, password);
+            resetFormField();
+        } catch (error) {
+            if (error.code === 'auth/wrong-password') {
+                alert('wrong password');
+            } else if (error.code === 'auth/user-not-found') {
+                alert('User not found');
+            } else {
+                console.log('user creation encountered an error', error);
+            }
+        }
+    };
+
+    const handleSignIn = async () => {
+        const { user } = await signInWithGooglePopup();
+        createUserDocumentFromAuth(user);
+    };
+
     return (
         <div className='sign-in-container'>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h2>Already have an account</h2>
                 <span>Sign In with yuur email and password</span>
                 <div>
@@ -32,7 +60,9 @@ const SignInForm = () => {
                 </div>
                 <div className='btn-container'>
                     <Button>Sign In</Button>
-                    <Button className='btn-google'>Sign In with google</Button>
+                    <Button type='button' className='btn-google' onClick={handleSignIn}>
+                        Sign In with google
+                    </Button>
                 </div>
             </form>
         </div>
--- src/routes/Auth/Auth.jsx
+++ src/routes/Auth/Auth.jsx
@@ -1,25 +1,9 @@
 import './Auth.scss';
-import React, { Fragment, useEffect } from 'react';
-import { getRedirectResult } from 'firebase/auth';
-import { auth, createUserDocumentFromAuth, signInWithGooglePopup, signInWithGoogleRedirect } from '../../utils/firebase/firebaseUtils';
+import React, { Fragment } from 'react';
 import SignUpForm from '../../components/SignUpForm/SignUpForm';
 import SignInForm from '../../components/SignInForm/SignInForm';
 
 const Auth = () => {
-    const handleSignIn = async () => {
-        const { user } = await signInWithGooglePopup();
-        createUserDocumentFromAuth(user);
-    };
-
-    useEffect(() => {
-        getRedirectResult(auth).then((response) => {
-            if (response) {
-                const { user } = response;
-                createUserDocumentFromAuth(user);
-            }
-        });
-    }, []);
-
     return (
         <Fragment>
             <div className='auth-container'>
