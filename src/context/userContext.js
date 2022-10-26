886d0cdfeefd2c01058c6b5414c456fbcde0797b
vaibhav.varshney <vaibhav.varshney@flipkart.com>
2022-10-26 14:21:31 +0530

    onstate changed added

--- src/context/userContext.js
+++ src/context/userContext.js
@@ -1,4 +1,5 @@
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
+import { onAuthStateChangedListener } from '../utils/firebase/firebaseUtils';
 
 export const UserContext = createContext({
     currentUser: null,
@@ -7,5 +8,12 @@ export const UserContext = createContext({
 export const UserProvider = ({ children }) => {
     const [currentUser, setCurrentUser] = useState(null);
     const value = { currentUser, setCurrentUser };
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChangedListener((user) => {
+            setCurrentUser(user);
+        });
+        return unsubscribe;
+    }, []);
     return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
--- src/utils/firebase/firebaseUtils.js
+++ src/utils/firebase/firebaseUtils.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from 'firebase/auth';
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
@@ -54,5 +54,7 @@ export const SignInAuthUserWithEmailPassword = async (email, password) => {
 
     return await signInWithEmailAndPassword(auth, email, password);
 };
+
+export const onAuthStateChangedListener = (callback) => onAuthStateChanged(auth, callback);
 
 export const SignOutUser = () => signOut(auth);
